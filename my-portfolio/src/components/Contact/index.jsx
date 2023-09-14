9fdffe95b249c6d2b4bfa46daef3f6c74fa4b617
Shabnamfazly <127684909+Shabnamfazly@users.noreply.github.com>
2023-09-14 15:12:06 +0100

    added styling to contact me page

--- my-portfolio/src/components/Contact/index.jsx
+++ my-portfolio/src/components/Contact/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import '../Contact/style.css'
+
 
 export default function Contact() {
   return (
-    <div> 
-      Contact Me
-      Email: Shabnamfazly@gmail.com
-      Github: https://github.com/Shabnamfazly
-      
-      </div>
-  )
+     <div className="contact-container">
+      <h1 className="contact-heading">Contact Me</h1>
+      <form className="contact-form">
+        <input
+          type="text"
+          className="contact-input"
+          placeholder="Your Name"
+        />
+        <input
+          type="email"
+          className="contact-input"
+          placeholder="Your Email"
+        />
+        <textarea
+          className="contact-textarea"
+          placeholder="Your Message"
+          rows="4"
+        ></textarea>
+        <button type="submit" className="contact-button">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
 }
+

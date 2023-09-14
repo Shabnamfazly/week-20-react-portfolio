f656c7aee53982efb7dc7d25d3e44684d2783554
Shabnamfazly <127684909+Shabnamfazly@users.noreply.github.com>
2023-09-14 15:37:38 +0100

    styling to page

--- my-portfolio/src/components/Footer/index.jsx
+++ my-portfolio/src/components/Footer/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import '../Footer/style.css'
+
 
 export default function Footer() {
-  return (
-    <div>
-      Shabnam Fazly Portfolio
-    
-    </div>
-  )
+ return (
+    <footer className="footer">
+      <div className="footer-links">
+        <a href="https://github.com/shabnamfazly" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i> GitHub
+        </a>
+        <a href="https://linkedin.com/in/shabnamfazly" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i> LinkedIn
+        </a>
+      </div>
+    </footer>
+  );
 }

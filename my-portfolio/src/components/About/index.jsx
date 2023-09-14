8e7e8a90909aa382e817ec5ede3136a84c3ba418
Shabnamfazly <127684909+Shabnamfazly@users.noreply.github.com>
2023-09-14 15:06:08 +0100

    styling to about me page

--- my-portfolio/src/components/About/index.jsx
+++ my-portfolio/src/components/About/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import "../About/style.css"
 
 export default function About() {
   return (
-    <div class='about'>Hello there! I'm Shabnam, a passionate and driven coding enthusiast on the cusp of completing an intensive coding bootcamp. My journey into the world of programming began as a curiosity and has evolved into a profound passion for crafting elegant and efficient solutions to real-world problems. Throughout this immersive bootcamp experience, I've honed my skills in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Theory, MongoDB, MySQL and Git.
+<div className="about-me-container">
+      <h1 className="about-me-heading">About Me</h1>
+      <p className="about-me-description">
+      Hello there! I'm Shabnam, a passionate and driven coding enthusiast on the cusp of completing an intensive coding bootcamp. My journey into the world of programming began as a curiosity and has evolved into a profound passion for crafting elegant and efficient solutions to real-world problems. Throughout this immersive bootcamp experience, I've honed my skills in HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Theory, MongoDB, MySQL and Git.
 
-    As I stand at the final stages of this exhilarating learning adventure, I am eager to showcase the projects and knowledge I've acquired along the way. I've collaborated on diverse projects, from building interactive web applications to creating data-driven solutions that analyze and visualize information. My dedication to clean code, attention to detail, and the relentless pursuit of excellence drives me to continuously expand my coding horizons.
-    
-    I am excited to embark on the next chapter of my coding journey, seeking opportunities to contribute my skills and make a meaningful impact in the tech industry. Whether it's building user-friendly interfaces, solving complex algorithmic puzzles, or diving into the latest technological trends, I am always up for the challenge. Join me on this exciting ride through my portfolio, where you can explore the projects that reflect my passion, creativity, and determination as I prepare to launch my career as a developer.</div>
-  )
+As I stand at the final stages of this exhilarating learning adventure, I am eager to showcase the projects and knowledge I've acquired along the way. I've collaborated on diverse projects, from building interactive web applications to creating data-driven solutions that analyze and visualize information. My dedication to clean code, attention to detail, and the relentless pursuit of excellence drives me to continuously expand my coding horizons.
+
+I am excited to embark on the next chapter of my coding journey, seeking opportunities to contribute my skills and make a meaningful impact in the tech industry. Whether it's building user-friendly interfaces, solving complex algorithmic puzzles, or diving into the latest technological trends, I am always up for the challenge. Join me on this exciting ride through my portfolio, where you can explore the projects that reflect my passion, creativity, and determination as I prepare to launch my career as a developer.
+      </p>
+    </div>
+  );
 }
+

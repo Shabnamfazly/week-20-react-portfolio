465c99b37d581b6ee89c7e446256da246b2e5779
Shabnamfazly <127684909+Shabnamfazly@users.noreply.github.com>
2023-09-14 16:00:44 +0100

    added styling to resume

--- my-portfolio/src/components/Resume/index.jsx
+++ my-portfolio/src/components/Resume/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import "../Resume/style.css"
 
 export default function Resume() {
+ const resumeUrl = 'https://pdf.ac/24A23v'; 
+
   return (
-    <div>Resume</div>
-  )
+    <div className="resume-container">
+      <h1 className="resume-heading">Resume</h1>
+      
+      <a
+        href={resumeUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="resume-link"
+      >
+        Click here to view my resume
+      </a>
+
+      <div className="resume-section">
+        
+      </div>
+    </div>
+  );
 }
+
